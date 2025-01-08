ee0e1bb62f01b8cd935ee5460f5e98de6b5847fe
lfdga <lfdga@ZENBOOK>
2025-01-08 23:00:31 +0000

    Add project files.

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,27 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IdentityExperiment
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources => [
+            new IdentityResources.OpenId()
+        ];
+
+        public static IEnumerable<ApiScope> ApiScopes => [
+            new(name: "weather-forecast", displayName: "Weather Forecast")
+        ];
+
+        public static IEnumerable<Client> Clients => [
+            new()
+            {
+                ClientId = "my-weather-app",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = [
+                    new("my-super-secret".Sha256())
+                ],
+                AllowedScopes = ["weather-forecast"]
+            }
+        ];
+    }
+}
